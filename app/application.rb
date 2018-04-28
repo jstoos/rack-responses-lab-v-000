c0d62c6408466ada8c6c5f51777eefe9b341c772
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-28 16:09:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     time = Time.now
 
     if time < 12:00
-       resp.write "Good Morning! #{Time.now}"
+       resp.write "Good Morning!"
     else
        resp.write "Good Afternoon!"
     end

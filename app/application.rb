2ce33591423fe9bf4980d68b16f3bc43ace0ae36
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-28 16:05:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,8 @@ class Application
     resp = Rack::Response.new
 
     # if Time.now < 12:00
-       resp.write "Good Morning! #{Time.now}"
-       "#{Time.now < 12}" 
+       resp.write "Good Morning! #{Timestamp}"
+       "#{Time.now.parse < 12}" 
     # else
     #   resp.write "Good Afternoon!"
     # end
